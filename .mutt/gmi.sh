7e398ccb8c8cf87276f49c7316d24b5108bee06b
Ryan Petrello <ryan@ryanpetrello.com>
2024-06-11 19:17:47 -0400

    more mail setup/automation

--- .mutt/gmi.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-~/Library/Python/3.11/bin/gmi sync
--- .mutt/idle.py
+++ .mutt/idle.py
@@ -5,8 +5,9 @@ import select
 import subprocess
 
 IDLE_FOLDERS = [
-    ("Ryan", "ryan@ryanpetrello.com", "INBOX", 'imap.fastmail.com'),
-    ("Ryan", "ryan@ryanpetrello.com", "Sent", 'imap.fastmail.com'),
+    #("Ryan", "ryan@ryanpetrello.com", "INBOX", 'imap.fastmail.com'),
+    #("Ryan", "ryan@ryanpetrello.com", "Sent", 'imap.fastmail.com'),
+    ("Mailchimp", "ryan_petrello@intuit.com", "INBOX", 'imap.gmail.com'),
 ]
 
 
